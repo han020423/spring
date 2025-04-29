f05677b73b5970f3ec38610ae8a8adac5b249ea3
jaehyuk <han020423@gmail.com>
2025-04-29 21:44:41 +0900

    댓글 엔티티 완료

---
+++ study1/src/main/java/com/example/study1/domain/comment/entity/Comment.java
@@ -0,0 +1,49 @@
+package com.example.study1.domain.comment.entity;
+
+import com.example.study1.domain.board.entity.Board;
+import com.example.study1.domain.member.entity.Member;
+import com.example.study1.global.tamplate.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "comments")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id", nullable = false)
+    private Board board;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Comment parent;
+
+    @Builder
+    private Comment(String content, Board board, Member member, Comment parent) {
+        this.content = content;
+        this.board = board;
+        this.member = member;
+        this.parent = parent;
+    }
+
+    public void updateComment(String content) {
+        this.content = content;
+    }
+}
---
+++ study1/src/main/java/com/example/study1/domain/comment/repository/CommentRepository.java
@@ -0,0 +1,4 @@
+package com.example.study1.domain.comment.repository;
+
+public interface CommentRepository {
+}
